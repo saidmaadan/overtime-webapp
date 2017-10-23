48be91038fcb1bb21d195e12862da0412e9cea34
Said Maadan <me@saidmaadan.com>
2017-10-22 22:28:38 -0500

    Implemented users association wit posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,10 +14,17 @@ describe "navigate" do
   end
 
   describe "creation" do
+    before do
+      user = User.create(email: "test@example.com", password:
+                          "password", password_confirmation: "password",
+                          first_name: 'Said', last_name: 'Fola'
+                          )
+    end
     it "has a new form tha that cab be reach" do
       visit new_post_path
       expect(page.status_code).to eq 200
     end
+
     it "can be created from new form page" do
       visit new_post_path
 
@@ -25,6 +32,15 @@ describe "navigate" do
       fill_in 'post[rationale]', with: "Post Software"
       click_on "Save"
       expect(page).to have_content("Post Software")
+    end
+
+    it 'will have a user associated with it' do
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "My Post"
+      click_on "Save"
+
+      expect(User.last.posts.rationale).to eq("My Post")
     end
   end
 end
