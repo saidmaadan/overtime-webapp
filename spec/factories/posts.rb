8d99c284175683183d2cd4342b4abb964e7a5020
Said Maadan <me@saidmaadan.com>
2017-10-24 22:33:49 -0500

    Implemented enum for status into posts for approval workflow

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "This is a post"
+    user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "This is a second post"
+    user
   end
 end
