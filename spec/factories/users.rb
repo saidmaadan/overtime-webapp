f2cae8b222f82c9ceb7b0a2a2dab20bb0df30dc2
Said Maadan <me@saidmaadan.com>
2017-10-25 12:21:31 -0500

    Implemented tests and page redirection for permission structure

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,20 +1,29 @@
 FactoryGirl.define do
-  sequence :email do |n|
-    'test#{n}@example.com'
+	sequence :email do |n|
+    "test#{n}@test.com"
   end
+
   factory :user do
     first_name 'Said'
     last_name 'Fola'
-    email {generate :email}
-    password 'password'
-    password_confirmation 'password'
+    email { generate :email }
+    password "password"
+    password_confirmation "password"
   end
 
   factory :admin_user, class: "AdminUser" do
     first_name 'Admin'
     last_name 'User'
-    email { generate :email}
-    password 'password'
-    password_confirmation 'password'
+    email { generate :email }
+    password "password"
+    password_confirmation "password"
+  end
+
+  factory :non_authorized_user, class: "User" do
+    first_name 'Non'
+    last_name 'Authorized'
+    email { generate :email }
+    password "password"
+    password_confirmation "password"
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,55 +1,102 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "navigate" do
+describe 'navigate' do
   before do
     @user = FactoryGirl.create(:user)
     login_as(@user, :scope => :user)
   end
 
   describe 'index' do
-    it "can be reached successfully" do
+    before do
       visit posts_path
-      expect(page.status_code).to eq(200)
     end
 
-    it "has a title of Posts" do
-      visit posts_path
-      expect(page).to have_content(/Posts/)
-    end
+  	it 'can be reached successfully' do
+  		expect(page.status_code).to eq(200)
+  	end
 
-    it "has a list of posts" do
+  	it 'has a title of Posts' do
+  		expect(page).to have_content(/Posts/)
+  	end
+
+    it 'has a list of posts' do
       post1 = FactoryGirl.build_stubbed(:post)
       post2 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
-
       expect(page).to have_content(/Rationale|content/)
-
     end
   end
 
-  describe "creation" do
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
+
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'creation' do
+  	before do
+  		visit new_post_path
+  	end
+
+  	it 'has a new form that can be reached' do
+  		expect(page.status_code).to eq(200)
+  	end
+
+  	it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+
+      expect(page).to have_content("Some rationale")
+  	end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
+
+  describe 'edit' do
     before do
-      visit new_post_path
-    end
-    it "has a new form tha that cab be reach" do
-      expect(page.status_code).to eq 200
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
-    it "can be created from new form page" do
+    it 'can be edited' do
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Post Software"
+      fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
-      expect(page).to have_content("Post Software")
+
+      expect(page).to have_content("Edited content")
     end
 
-    it 'will have a user associated with it' do
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
 
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "My Post"
-      click_on "Save"
+      visit edit_post_path(@edit_post)
 
-      expect(User.last.posts.rationale).to eq("My Post")
+      expect(current_path).to eq(root_path)
     end
   end
 end
