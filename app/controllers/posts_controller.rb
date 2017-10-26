a20a051e6f13e77cd4968c8d1e9ae7736041f42d
Said Maadan <me@saidmaadan.com>
2017-10-25 22:16:59 -0500

    Modified index method for displaying only user created posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :edit, :destroy]
   def index
-    @posts = Post.all
+    @posts = current_user.posts
   end
 
   def new
